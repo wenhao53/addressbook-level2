a8fa89c7002a9f2033e1db58de96ec6c351d5bdc
Wenhao Cheng <wenhao.cheng@u.nus.edu>
2018-02-15 14:38:56 +0800

    Added dummy sort command

--- src/seedu/addressbook/commands/HelpCommand.java
+++ src/seedu/addressbook/commands/HelpCommand.java
@@ -21,6 +21,7 @@ public class HelpCommand extends Command {
                 + "\n\n" + ClearCommand.MESSAGE_USAGE
                 + "\n\n" + FindCommand.MESSAGE_USAGE
                 + "\n\n" + ListCommand.MESSAGE_USAGE
+                + "\n\n" + SortCommand.MESSAGE_USAGE
                 + "\n\n" + ViewCommand.MESSAGE_USAGE
                 + "\n\n" + ViewAllCommand.MESSAGE_USAGE
                 + "\n\n" + HelpCommand.MESSAGE_USAGE
